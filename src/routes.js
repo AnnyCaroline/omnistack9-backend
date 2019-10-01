9bf4f9ab033a5c2bc7388bd0e0035a0f2e598c2f
AnnyCaroline <annycarolinegnr@gmail.com>
2019-10-01 18:38:40 -0300

    add show dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
+const DashboardController = require('./controllers/DashboardController');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
@@ -9,7 +10,10 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store); //se fossem várias imagens, poderia usar upload.arr()
+
+routes.get('/dashboards', DashboardController.show);
 
 module.exports = routes;
