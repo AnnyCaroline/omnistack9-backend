4d6a426a323e3dc5ef967459216cab610018aa2b
AnnyCaroline <annycarolinegnr@gmail.com>
2019-10-01 18:49:39 -0300

    add store booking

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const DashboardController = require('./controllers/DashboardController');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -15,5 +16,7 @@ routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store); //se fossem várias imagens, poderia usar upload.arr()
 
 routes.get('/dashboards', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
